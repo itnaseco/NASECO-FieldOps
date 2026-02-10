5c7b38f2a6f7d3b5604f061946555295e134e179
Matovu Matthew <itnaseco@gmail.com>
2026-02-10 11:22:54 +0300

    recipe input and stage handling

--- naseco_fieldopsbackend/naseco_fieldopsbackend/doctype/crop_recipe/crop_recipe.js
+++ naseco_fieldopsbackend/naseco_fieldopsbackend/doctype/crop_recipe/crop_recipe.js
@@ -1,8 +1,70 @@
 // Copyright (c) 2026, Naseco and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Crop Recipe", {
-// 	refresh(frm) {
+function set_stage_options(frm) {
+  const stages = (frm.doc.stages || [])
+    .map((s) => s.stage_name)
+    .filter((v) => v);
+  const options = stages.join("\n");
 
-// 	},
-// });
+  const grid = frm.fields_dict.inputs && frm.fields_dict.inputs.grid;
+  if (grid) {
+    const df = grid.get_field("recipe_stage");
+    if (df) {
+      df.options = options;
+    }
+    grid.refresh();
+  }
+}
+
+function sync_input_stage_indexes(frm) {
+  const stageIndexByName = {};
+  (frm.doc.stages || []).forEach((s) => {
+    if (s.stage_name) {
+      stageIndexByName[s.stage_name] = s.order_index || s.idx;
+    }
+  });
+
+  (frm.doc.inputs || []).forEach((row) => {
+    if (row.recipe_stage && stageIndexByName[row.recipe_stage]) {
+      row.stage_index = stageIndexByName[row.recipe_stage];
+    }
+  });
+
+  frm.refresh_field("inputs");
+}
+
+frappe.ui.form.on("Crop Recipe", {
+  refresh(frm) {
+    set_stage_options(frm);
+    sync_input_stage_indexes(frm);
+  },
+  stages_add(frm) {
+    set_stage_options(frm);
+    sync_input_stage_indexes(frm);
+  },
+  stages_remove(frm) {
+    set_stage_options(frm);
+    sync_input_stage_indexes(frm);
+  },
+});
+
+frappe.ui.form.on("Recipe Stage", {
+  stage_name(frm) {
+    set_stage_options(frm);
+    sync_input_stage_indexes(frm);
+  },
+  order_index(frm) {
+    sync_input_stage_indexes(frm);
+  },
+});
+
+frappe.ui.form.on("Recipe Input Item", {
+  recipe_stage(frm, cdt, cdn) {
+    const row = locals[cdt][cdn];
+    const stages = frm.doc.stages || [];
+    const match = stages.find((s) => s.stage_name === row.recipe_stage);
+    row.stage_index = match ? match.order_index || match.idx : null;
+    frm.refresh_field("inputs");
+  },
+});
--- naseco_fieldopsbackend/naseco_fieldopsbackend/doctype/crop_recipe/crop_recipe.py
+++ naseco_fieldopsbackend/naseco_fieldopsbackend/doctype/crop_recipe/crop_recipe.py
@@ -1,9 +1,27 @@
 # Copyright (c) 2026, Naseco and contributors
 # For license information, please see license.txt
 
-# import frappe
 from frappe.model.document import Document
 
 
 class CropRecipe(Document):
-	pass
+	def validate(self):
+		stage_index = {}
+		stage_name_by_row = {}
+		for s in self.stages or []:
+			if s.stage_name:
+				stage_index[s.stage_name] = s.order_index or s.idx
+			if s.name and s.stage_name:
+				stage_name_by_row[s.name] = s.stage_name
+
+		for row in self.inputs or []:
+			# Migrate old rowname link to stage name if needed
+			if row.recipe_stage in stage_name_by_row:
+				row.recipe_stage = stage_name_by_row.get(row.recipe_stage)
+
+			# Default recipe_stage from input_type if missing
+			if not row.recipe_stage and row.input_type in stage_index:
+				row.recipe_stage = row.input_type
+
+			if row.recipe_stage in stage_index:
+				row.stage_index = stage_index.get(row.recipe_stage)
