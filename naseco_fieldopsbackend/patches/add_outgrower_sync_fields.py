c7cbc0d3b18ff3bbd8668d63e7e0be266ecc7e87
Matovu Matthew <itnaseco@gmail.com>
2026-02-19 16:20:53 +0300

    Bank Account and Outgrower Type Fix

--- naseco_fieldopsbackend/patches/add_outgrower_sync_fields.py
+++ naseco_fieldopsbackend/patches/add_outgrower_sync_fields.py
@@ -4,22 +4,30 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 def execute():
 	"""Idempotently add sync-compatible fields to Outgrower."""
-	custom_fields = {
-		"Outgrower": [
+	meta = frappe.get_meta("Outgrower")
+	to_create = []
+
+	if not meta.has_field("bank_account"):
+		to_create.append(
 			{
 				"fieldname": "bank_account",
 				"label": "Bank Account",
 				"fieldtype": "Data",
 				"insert_after": "assigned_supervisor",
-			},
+			}
+		)
+
+	if not meta.has_field("outgrower_type"):
+		to_create.append(
 			{
 				"fieldname": "outgrower_type",
 				"label": "Outgrower Type",
 				"fieldtype": "Select",
 				"options": "Individual\nGroup\nCooperative\nCompany",
 				"insert_after": "bank_account",
-			},
-		]
-	}
-	create_custom_fields(custom_fields, update=True)
+			}
+		)
+
+	if to_create:
+		create_custom_fields({"Outgrower": to_create}, update=True)
 	frappe.db.commit()
